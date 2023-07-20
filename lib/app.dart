4f222214f9be20d04200d3dbcaab926d1ebb09f9
Halawany1 <mdnjdhdhd44@gmail.com>
2023-07-20 13:00:41 +0300

    finished project1

--- lib/app.dart
+++ lib/app.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 
 
 
-class appScreenState extends StatelessWidget {
+class AppScreenState extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const Placeholder();
