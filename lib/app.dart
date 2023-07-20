b8fff95f5fed2f651c4ee9cbdccf286938057aee
Omar_Ameer <omar.ameer244@gmail.com>
2023-07-20 13:01:59 +0300

    commit 2

--- lib/app.dart
+++ lib/app.dart
@@ -1,14 +1,9 @@
 import 'package:flutter/material.dart';
 
 
-class AppScreen extends StatefulWidget {
+class AppScreen extends StatelessWidget {
   const AppScreen({super.key});
 
-  @override
-  State<AppScreen> createState() => _AppScreenState();
-}
-
-class _AppScreenState extends State<AppScreen> {
   @override
   Widget build(BuildContext context) {
     return const Placeholder();
